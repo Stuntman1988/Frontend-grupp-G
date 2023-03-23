2048b0e513380c765ca92cd3005ae1dde373fee1
Robin lundell <robin.lundell@hotmail.com>
2023-03-23 10:42:39 +0100

    upd

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function myFunction(id) {
 }
 
 
-fetch('https://fakestoreapi.com/products')
+fetch('https://mocki.io/v1/a99e6cf4-1e5a-4b0e-bc57-6c651f0f09cd')
     .then(res => {
         return res.json();
     })
